2df5e70f92b01bb4e358ad27169dadd6b491a2d6
hamza-nasser <hamzanasser249@gmail.com>
2023-03-22 20:57:37 +0200

    [presentation] - Initialize main layout with home pane

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -1,11 +1,59 @@
-import 'package:flutter/material.dart';
+import 'package:application/presentation/screens/home/home.dart';
+import 'package:application/presentation/screens/settings/settings.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_acrylic/flutter_acrylic.dart' as flutter_acrylic;
+import 'package:application/theme.dart';
+import 'package:fluent_ui/fluent_ui.dart';
 
-void main() {
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:system_theme/system_theme.dart';
+import 'package:window_manager/window_manager.dart';
+
+const String appTitle = 'Bedaya Convey Management System';
+
+/// Checks if the current environment is a desktop environment.
+bool get isDesktop {
+  if (kIsWeb) return false;
+  return [
+    TargetPlatform.windows,
+    TargetPlatform.linux,
+    TargetPlatform.macOS,
+  ].contains(defaultTargetPlatform);
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   // For performance (UI smoothness) debug (Profile mode)
   // Note: there are various type of performance like
   PerformanceOverlay.allEnabled(
     checkerboardOffscreenLayers: true,
   );
+  if (!kIsWeb &&
+      [
+        TargetPlatform.windows,
+        TargetPlatform.android,
+      ].contains(defaultTargetPlatform)) {
+    SystemTheme.accentColor.load();
+  }
+
+  if (isDesktop) {
+    await flutter_acrylic.Window.initialize();
+    await flutter_acrylic.Window.hideWindowControls();
+    await WindowManager.instance.ensureInitialized();
+    windowManager.waitUntilReadyToShow().then((_) async {
+      await windowManager.setTitleBarStyle(
+        TitleBarStyle.hidden,
+        windowButtonVisibility: false,
+      );
+      await windowManager.setSize(const Size(755, 545));
+      await windowManager.setMinimumSize(const Size(350, 600));
+      await windowManager.center();
+      await windowManager.show();
+      await windowManager.setPreventClose(true);
+      await windowManager.setSkipTaskbar(false);
+    });
+  }
   runApp(const App());
 }
 
@@ -16,26 +64,275 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(brightness: Brightness.dark),
-      darkTheme: ThemeData(brightness: Brightness.dark),
-      themeMode: ThemeMode.dark,
-      debugShowCheckedModeBanner: false,
-      debugShowMaterialGrid: false,
-      home: const HomePage(),
+    return ChangeNotifierProvider(
+      create: (context) => AppTheme(),
+      builder: (context, state) {
+        final appTheme = context.watch<AppTheme>();
+        return FluentApp.router(
+          routeInformationParser: router.routeInformationParser,
+          routerDelegate: router.routerDelegate,
+          color: appTheme.color,
+          builder: (context, child) {
+            return Directionality(
+              textDirection: appTheme.textDirection,
+              child: NavigationPaneTheme(
+                data: NavigationPaneThemeData(
+                  backgroundColor: appTheme.windowEffect !=
+                          flutter_acrylic.WindowEffect.disabled
+                      ? Colors.transparent
+                      : null,
+                ),
+                child: child!,
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomeLayout extends StatefulWidget {
+  const HomeLayout({
+    super.key,
+    required this.child,
+    required this.shellContext,
+    required this.state,
+  });
+  final Widget child;
+  final BuildContext? shellContext;
+  final GoRouterState state;
+
+  @override
+  State<HomeLayout> createState() => _HomeLayoutState();
+}
+
+class _HomeLayoutState extends State<HomeLayout> {
+  bool smallScreenWidth = false;
+  bool isMobile = false;
+  final viewKey = GlobalKey(debugLabel: 'Navigation View Key');
+  final List<NavigationPaneItem> originalItems = [
+    PaneItem(
+      key: const Key('/'),
+      icon: const Icon(FluentIcons.home),
+      title: const Text('Home'),
+      body: const SizedBox.shrink(),
+      onTap: () {
+        if (router.location != '/') router.pushNamed('home');
+      },
+    ),
+    PaneItemHeader(header: const Text('Inputs')),
+  ];
+  final List<NavigationPaneItem> footerItems = [
+    PaneItemSeparator(),
+    PaneItem(
+      key: const Key('/settings'),
+      icon: const Icon(FluentIcons.settings),
+      title: const Text('Settings'),
+      body: const SizedBox.shrink(),
+      onTap: () {
+        if (router.location != '/settings') {
+          router.pushNamed('settings');
+        }
+      },
+    ),
+  ];
+  int _calculateSelectedIndex(BuildContext context) {
+    final location = router.location;
+    int indexOriginal = originalItems
+        .where((element) => element.key != null)
+        .toList()
+        .indexWhere((element) => element.key == Key(location));
+
+    if (indexOriginal == -1) {
+      int indexFooter = footerItems
+          .where((element) => element.key != null)
+          .toList()
+          .indexWhere((element) => element.key == Key(location));
+      if (indexFooter == -1) {
+        return 0;
+      }
+      return originalItems
+              .where((element) => element.key != null)
+              .toList()
+              .length +
+          indexFooter;
+    } else {
+      return indexOriginal;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home page'),
+    final localizations = FluentLocalizations.of(context);
+
+    final appTheme = context.watch<AppTheme>();
+    final theme = FluentTheme.of(context);
+    if (widget.shellContext != null) {
+      if (router.canPop() == false) {
+        setState(() {});
+      }
+    }
+    return NavigationView(
+      appBar: NavigationAppBar(
+        automaticallyImplyLeading: false,
+        leading: smallScreenWidth
+            ? null
+            : () {
+                final enabled = widget.shellContext != null && router.canPop();
+
+                final onPressed = enabled
+                    ? () {
+                        if (router.canPop()) {
+                          context.pop();
+                          setState(() {});
+                        }
+                      }
+                    : null;
+                return NavigationPaneTheme(
+                  data: NavigationPaneTheme.of(context).merge(
+                    NavigationPaneThemeData(
+                      unselectedIconColor: ButtonState.resolveWith((states) {
+                        if (states.isDisabled) {
+                          return ButtonThemeData.buttonColor(context, states);
+                        }
+                        return ButtonThemeData.uncheckedInputColor(
+                          FluentTheme.of(context),
+                          states,
+                        ).basedOnLuminance();
+                      }),
+                    ),
+                  ),
+                  //
+                  child: smallScreenWidth
+                      ? const SizedBox.shrink()
+                      : Builder(
+                          builder: (context) => PaneItem(
+                            icon: const Center(
+                                child: Icon(FluentIcons.back, size: 12.0)),
+                            title: Text(localizations.backButtonTooltip),
+                            body: const SizedBox.shrink(),
+                            enabled: enabled,
+                          ).build(
+                            context,
+                            false,
+                            onPressed,
+                            displayMode: PaneDisplayMode.compact,
+                          ),
+                        ),
+                );
+              }(),
+        title: () {
+          if (kIsWeb) {
+            return const Align(
+              alignment: AlignmentDirectional.centerStart,
+              child: Text(appTitle),
+            );
+          }
+          return smallScreenWidth
+              ? null
+              : const DragToMoveArea(
+                  child: Align(
+                    alignment: AlignmentDirectional.centerStart,
+                    child: Text(appTitle),
+                  ),
+                );
+        }(),
+        actions: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+          Padding(
+            padding:
+                EdgeInsetsDirectional.only(end: 8.0, top: isMobile ? 10 : 0),
+            child: ToggleSwitch(
+              content: const Text('Dark Mode'),
+              checked: FluentTheme.of(context).brightness.isDark,
+              onChanged: (v) {
+                if (v) {
+                  appTheme.mode = ThemeMode.dark;
+                } else {
+                  appTheme.mode = ThemeMode.light;
+                }
+              },
+            ),
+          ),
+          if (!kIsWeb && !isMobile) const WindowButtons(),
+        ]),
+      ),
+      paneBodyBuilder: (item, child) {
+        final name =
+            item?.key is ValueKey ? (item!.key as ValueKey).value : null;
+        return FocusTraversalGroup(
+          key: ValueKey('body$name'),
+          child: widget.child,
+        );
+      },
+      pane: NavigationPane(
+        items: originalItems,
+        footerItems: footerItems,
+        selected: _calculateSelectedIndex(context),
+        header: SizedBox(
+          height: kOneLineTileHeight,
+          child: ShaderMask(
+            shaderCallback: (rect) {
+              final color = appTheme.color.defaultBrushFor(
+                theme.brightness,
+              );
+              return LinearGradient(
+                colors: [
+                  color,
+                  color,
+                ],
+              ).createShader(rect);
+            },
+            child: const FlutterLogo(
+              style: FlutterLogoStyle.horizontal,
+              size: 80.0,
+              textColor: Colors.white,
+              duration: Duration.zero,
+            ),
+          ),
+        ),
       ),
     );
   }
 }
+
+class WindowButtons extends StatelessWidget {
+  const WindowButtons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final FluentThemeData theme = FluentTheme.of(context);
+
+    return SizedBox(
+      width: 138,
+      height: 50,
+      child: WindowCaption(
+        brightness: theme.brightness,
+        backgroundColor: Colors.transparent,
+      ),
+    );
+  }
+}
+
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
+final _shellNavigatorKey = GlobalKey<NavigatorState>();
+final GoRouter router = GoRouter(navigatorKey: _rootNavigatorKey, routes: [
+  ShellRoute(
+      navigatorKey: _shellNavigatorKey,
+      builder: (context, state, child) {
+        return HomeLayout(
+          shellContext: _shellNavigatorKey.currentContext,
+          state: state,
+          child: child,
+        );
+      },
+      routes: [
+        GoRoute(
+            path: '/', name: 'home', builder: (context, state) => const Home()),
+        GoRoute(
+            path: '/settings',
+            name: 'settings',
+            builder: (context, state) => const Settings()),
+      ]),
+  GoRoute(path: '/', builder: (context, state) => const Home())
+]);
