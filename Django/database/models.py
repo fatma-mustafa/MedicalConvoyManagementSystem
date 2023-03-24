e4add7be4f60a89e00d7446d44fdb1bd857466d4
hazemAzzam <61450444+hazemAzzam@users.noreply.github.com>
2023-03-24 16:50:16 +0200

    test

---
+++ Django/database/models.py
@@ -0,0 +1,102 @@
+from django.db import models
+from .utils import *
+import json
+from django.contrib.postgres.fields import JSONField
+from .primary_key import Code
+
+class Family_History(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class Contraception_Info_Method(models.Model):
+    method = models.ManyToManyField(Contraception_Method)
+
+class Contraception_Info(models.Model):
+    contraception = models.BooleanField(default=False)
+    methods = models.ManyToManyField(Contraception_Method)
+
+# Create your models here.
+class Patient(models.Model):
+    code = models.CharField(max_length=10, blank=True, editable=False)
+    first_name = models.CharField(max_length=50, default='')
+    last_name = models.CharField(max_length=50, default='')
+    gender = models.CharField(max_length=6, choices=[('male', 'Male'), ('female', 'Female')])
+    date_of_birth = models.DateField(null=True)
+    occupation = models.CharField(max_length=50, default='')
+    mobile_number = models.CharField(max_length=11, null=True)
+    #marital_status = models.CharField(max_length=50, choices=[('single', 'Single'), ('married', 'Married'), ('divorced', 'Divorced'), ('widowed', 'Widowed')], default='single')
+    #marriage = models.JSONField(default=dict, null=True)
+    education_level = models.CharField(max_length=50, choices=[('illiterate', 'Illiterate'), ('read and write', 'Read and Write'), ('primary', 'Primary'), ('preparatory', 'Preparatory'), ('secondary', 'Secondary'), ('university', 'University'), ('postgraduate', 'Postgraduate')], default='illiterate')
+    
+    habits_of_medical_importance = models.JSONField(default=dict, null=True)
+    complaints = models.JSONField(default=dict, null=True)
+
+    past_history = models.JSONField(default=dict, null=True)
+    family_history = models.JSONField(default=dict, null=True)
+    
+    
+    def __str__(self):
+        return f"{self.code}: {self.first_name} {self.last_name}" 
+    
+class Marriage(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    marital_status = models.CharField(max_length=20, choices=[
+        ('single','Single'), 
+        ('married', 'Married'), 
+        ('divoced', 'Divorced'), 
+        ('windowed','Widowed')
+    ])
+    def __str__(self):
+        return f"{self.patient} {self.marital_status}"
+    
+class Marriage_Info(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    number_of_children = models.IntegerField()
+    age_of_the_youngest = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.patient} has {self.number_of_children} child, the youngest is {self.age_of_the_youngest}'s year"
+
+
+#class Complaint(models.Model):
+#    symptom = models.CharField(max_length=50)
+#    code = models.ForeignKey(Patient, on_delete=models.CASCADE)
+
+class UrinAnalysisReport(models.Model):
+    status = models.CharField(max_length=10, choices=[('pending', 'Pending'), ('finished', 'Finished'), ('cancelled', 'Cancelled')])
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    report_date = models.DateField(auto_now_add=True)
+    lab_code = models.CharField(max_length=50)
+    colour = models.CharField(max_length=20)
+    aspect = models.CharField(max_length=20)
+    blood = models.CharField(max_length=20)
+    urobilinogen = models.CharField(max_length=20)
+    bilirubin = models.CharField(max_length=20)
+    protien = models.CharField(max_length=20)
+    nitrite = models.CharField(max_length=20)
+    ketone_bodies = models.CharField(max_length=20)
+    glucose = models.CharField(max_length=20)
+    ph_level = models.CharField(max_length=20)
+    specific_gravity = models.CharField(max_length=20)
+    leukocyte_esterase = models.CharField(max_length=20)
+    rbcs = models.CharField(max_length=20)
+    pus_cells = models.CharField(max_length=20)
+    epithelial_cells = models.CharField(max_length=20)
+    calcium_oxalate = models.CharField(max_length=20)
+    uric_acid = models.CharField(max_length=20)
+    triple_phosphate = models.CharField(max_length=20)
+    amorphous = models.CharField(max_length=20)
+    microscopic_findings = models.TextField(null=True, blank=True)
+    additional_comments = models.TextField(null=True, blank=True)
+    lab_member_signature = models.CharField(max_length=50)
+    casts = models.CharField(max_length=20)
+    eggs = models.CharField(max_length=20)
+    mucus = models.CharField(max_length=20)
+    bacteria = models.CharField(max_length=20)
+    yeast = models.CharField(max_length=20)
+    sperm = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return f"{self.patient.first_name} {self.patient.last_name} - Urinalysis Report ({self.report_date})"
---
+++ Django/database/serializers.py
@@ -0,0 +1,34 @@
+from .models import *
+from rest_framework import serializers
+from .utils import *
+from .custom_serializer import custom_serializer
+
+class PatientSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Patient
+        fields = "__all__"
+
+class MarriageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Marriage
+        fields = "__all__"
+
+class UrinanalysisReportSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = UrinAnalysisReport
+        fields = "__all__"
+
+class Family_History_Serializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Family_History
+        fields = "__all__"
+
+class Contraception_Method_Serializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Contraception_Method
+        fields = "__all__"
+
+#class Contraception_Info_Serializer(serializers.HyperlinkedModelSerializer):
+#    class Meta:
+#        model = Contraception_Info
+#        fields = "__all__"
